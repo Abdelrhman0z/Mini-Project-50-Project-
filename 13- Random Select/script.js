39ffe9115109c15129a74a04b43eb551c99f1372
Abdelrhman Soliman <97921387+Abdelrhman0z@users.noreply.github.com>
2022-11-03 20:16:18 +0200

    Update script.js

--- 13- Random Select/script.js
+++ 13- Random Select/script.js
@@ -18,7 +18,6 @@ textarea.addEventListener('keyup', (e) => {
 
 function createTags(input) {
   const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-  console.log(tags)
 
   tagsEl.innerHTML = ''
 
