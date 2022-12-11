dba719fe21c67e32141320f2489c5ea44ac5a7e2
Abdelrhman Soliman <abdelrhmanmohamed927@gmail.com>
2022-12-11 18:04:42 +0200

    highlighting when a link is clicked

--- 25-Sticky Navbar/script.js
+++ 25-Sticky Navbar/script.js
@@ -1,5 +1,17 @@
 const nav = document.querySelector('.nav')
+const links = document.querySelectorAll('li a')
 
 window.addEventListener('scroll', fixNav)
 
 function fixNav() { (window.scrollY > nav.offsetHeight + 200) ? nav.classList.add('active') : nav.classList.remove('active') }
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    removeCurrent();
+    link.classList.add('current')
+  })
+})
+
+function removeCurrent() {
+  links.forEach(link => link.classList.remove('current'));
+}
