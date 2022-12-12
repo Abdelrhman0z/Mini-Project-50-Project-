6aad2f1c96d5af57e1bd8a39af7f49d98abb2cc8
Abdelrhman Soliman <abdelrhmanmohamed927@gmail.com>
2022-12-12 18:43:55 +0200

    Replace Slider Height

--- 26-Vertical Slider/script.js
+++ 26-Vertical Slider/script.js
@@ -7,29 +7,28 @@ const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-// slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
-// upButton.addEventListener('click', () => changeSlide('up'))
-// downButton.addEventListener('click', () => changeSlide('down'))
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
-// const changeSlide = (direction) => {
-//   const sliderHeight = sliderContainer.clientHeight
-//   if (direction === 'up') {
-//     activeSlideIndex++
-//   }
-//   if (activeSlideIndex > slidesLength - 1) {
-//     activeSlideIndex = 0
-//   }
+const changeSlide = (direction) => {
+  if (direction === 'up') {
+    activeSlideIndex++
+  }
+  if (activeSlideIndex > slidesLength - 1) {
+    activeSlideIndex = 0
+  }
 
-//   else if (direction === 'down') {
-//     activeSlideIndex--
-//     if (activeSlideIndex < 0) {
-//       activeSlideIndex = slidesLength - 1
-//     }
-//   }
+  else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1
+    }
+  }
 
 
-//   slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+  slideRight.style.transform = `translateY(-${activeSlideIndex * 100}vh)`
 
-//   slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-// }
+  slideLeft.style.transform = `translateY(${activeSlideIndex * 100}vh)`
+}
