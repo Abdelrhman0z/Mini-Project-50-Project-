5b2d24e51b70fe7b653b83260eadd0217fe80dbb
Abdelrhman Soliman <abdelrhmanmohamed927@gmail.com>
2023-01-15 20:04:30 +0200

    Remove wrapping  arrow function

--- 32-Toggle CheckBox/script.js
+++ 32-Toggle CheckBox/script.js
@@ -3,11 +3,11 @@ const good = document.querySelector('#good')
 const cheap = document.querySelector('#cheap')
 const fast = document.querySelector('#fast')
 
-toggles.forEach(toggle => toggle.addEventListener('change', (e) => {
-  cantHaveItAll(e.target)
-}))
+toggles.forEach(toggle => toggle.addEventListener('change', cantHaveItAll))
 
-function cantHaveItAll(theClickedOne) {
+
+function cantHaveItAll(e) {
+  const theClickedOne = e.target
   if (good.checked && cheap.checked && fast.checked) {
     if (good === theClickedOne) {
       fast.checked = false
