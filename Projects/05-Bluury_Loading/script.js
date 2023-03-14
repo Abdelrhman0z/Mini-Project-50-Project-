10a105275cf372b45be4d615b567a0e357cffb15
Abdelrhman Soliman <abdelrhmanmohamed927@gmail.com>
2023-03-14 20:00:25 +0200

    Organize System File

--- Projects/05-Bluury_Loading/script.js
+++ Projects/05-Bluury_Loading/script.js
@@ -1,7 +1,7 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
-let load = 0;
+let load = 0
 
 let int = setInterval(blurring, 30)
 
@@ -15,9 +15,8 @@ function blurring() {
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-
 }
 
-function scale(number, inMin, inMax, outMin, outMax) {
-  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
---
+++ script.js
@@ -0,0 +1,395 @@
+const projects = [
+  {
+    title: 'Expanding Card',
+    description: 'Quick Fun Project About The Most Visited Places',
+    imageUrl: './img/01.PNG',
+    link: './Projects/01-Expending_Cards/index.html',
+    number: '01'
+  },
+  {
+    title: 'Progress Steps',
+    description: 'A Bar That Move Forward As Long As You Doing A Mission',
+    imageUrl: './img/02.PNG',
+    link: './Projects/02-Progress_Steps/index.html',
+    number: '02'
+  },
+  {
+    title: 'Rotating Navigation',
+    description: 'A Rotating Navigation',
+    imageUrl: './img/03.PNG',
+    link: './Projects/03-Rotating_Navigation/index.html',
+    number: '03'
+  },
+  {
+    title: 'Hidden Search',
+    description: 'Hidden Search Is An Icon When You Click It Appears The Search Input ',
+    imageUrl: './img/04.PNG',
+    link: './Projects/04-Hidden_Search/index.html',
+    number: '04'
+  },
+  {
+    title: 'Blurry Loading',
+    description: ' A Count Down Blurry Loading From 100% to 0% ',
+    imageUrl: './img/05.PNG',
+    link: './Projects/05-Blurry_Loading/index.html',
+    number: '05'
+  },
+  {
+    title: 'Scroll Animation',
+    description: ' Scroll Animation Make Boxes Comes From The Edge Of The Screen As You Scroll ',
+    imageUrl: './img/06.PNG',
+    link: './Projects/06-Scroll_Animation/index.html',
+    number: '06'
+  },
+  {
+    title: 'Split Landing Page',
+    description: ' Split The Page Into Two Sides ',
+    imageUrl: './img/07.PNG',
+    link: './Projects/07-Split_Landing_Page/index.html',
+    number: '07'
+  },
+  {
+    title: 'Wave Animation',
+    description: ' Form When You Click On The Input The letters Make A Nice Wave Animation ',
+    imageUrl: './img/08.PNG',
+    link: './Projects/08-Form_Wave_Animation/index.html',
+    number: '08'
+  },
+  {
+    title: 'Sound Board',
+    description: ' 5 Boxes Contains Five Sound When You Click At Any Box You Can Hear The Sound',
+    imageUrl: './img/09.PNG',
+    link: './Projects/09-Sound_Board/index.html',
+    number: '09'
+  },
+  {
+    title: 'Dad Joke',
+    description: 'Button Every Time You Click It Fetch A Different Joke',
+    imageUrl: './img/10.PNG',
+    link: './Projects/10-Dad_Joke/index.html',
+    number: '10'
+  },
+  {
+    title: 'Event KeyCodes',
+    description: 'With Every KeyPress You Do It On The Keyboard Its Gonna Show up On The Box',
+    imageUrl: './img/11.PNG',
+    link: './Projects/11-Event_KeyCodes/index.html',
+    number: '11'
+  },
+  {
+    title: 'Toggle Buttons',
+    description: 'Toggle Questions Buttons With Answers Inside It ',
+    imageUrl: './img/12.PNG',
+    link: './Projects/12-Toggle_Buttons/index.html',
+    number: '12'
+  },
+  {
+    title: 'Random Select',
+    description: 'Input You Can Put Inside It Few Different Texts And Only One Will Be Selected Randomly',
+    imageUrl: './img/13.PNG',
+    link: './Projects/13-Random_Select/index.html',
+    number: '13'
+  },
+  {
+    title: 'Animated Navigation',
+    description: 'Animated Navigation That Expand With A Nice Effect When You Click To Close It Or Open It',
+    imageUrl: './img/14.PNG',
+    link: './Projects/14-Animated_Navigation/index.html',
+    number: '14'
+  },
+  {
+    title: 'Incrementing Counter',
+    description: 'Incrementing Counter That Shows Up When You Enter The Page ',
+    imageUrl: './img/15.PNG',
+    link: './Projects/15-Incrementing_Counter/index.html',
+    number: '15'
+  },
+  {
+    title: 'Drink Water',
+    description: 'Few Bottles Every Time You Click One You Also Filing Up A Big Cup Tells You How Much Water You Drink',
+    imageUrl: './img/16.PNG',
+    link: './Projects/16-Drink_Water/index.html',
+    number: '16'
+  },
+  {
+    title: 'Movie App',
+    description: ' Browse Movies And See Information About Them And The Rating And Much More',
+    imageUrl: './img/17.PNG',
+    link: './Projects/17-Movie_App/index.html',
+    number: '17'
+  },
+  {
+    title: 'Background Slider',
+    description: 'Background Slider Is A Small Image Gallery You Can Preview',
+    imageUrl: './img/18.PNG',
+    link: './Projects/18-Background_Slider/index.html',
+    number: '18'
+  },
+  {
+    title: 'Theme Clock',
+    description: 'Theme Clock Is Small Widget Screen That Shows You Time And Date With Good Look',
+    imageUrl: './img/19.PNG',
+    link: './Projects/19-Theme_Clock/index.html',
+    number: '19'
+  },
+  {
+    title: 'Button Ripple Effect',
+    description: 'Button That Show A Nice Ripple Effect Anywhere Whey You Click Inside The Button',
+    imageUrl: './img/20.PNG',
+    link: './Projects/20-Button_Ripple_Effect/index.html',
+    number: '20'
+  },
+  {
+    title: 'Drag N Drop',
+    description: 'Small Boxes That You Can Move A Picture Anywhere Between Them ',
+    imageUrl: './img/21.PNG',
+    link: './Projects/21-Drag_N_Drop/index.html',
+    number: '21'
+  },
+  {
+    title: 'Drawing App',
+    description: 'Box You Can Draw Anything You Want With Any Color You Want Just Like Any Drawing App',
+    imageUrl: './img/22.PNG',
+    link: './Projects/22-Drawing_App/index.html',
+    number: '22'
+  },
+  {
+    title: 'Kinetic Loader',
+    description: 'A Nice Animation With Two Triangles Moving With Each Other ',
+    imageUrl: './img/23.PNG',
+    link: './Projects/23-Kinetic_Loader/index.html',
+    number: '23'
+  },
+  {
+    title: 'Content Placeholder',
+    description: 'Card With User Information And Waiting Load Until User Data Being Fetched',
+    imageUrl: './img/24.PNG',
+    link: './Projects/24-Content_Placeholder/index.html',
+    number: '24'
+  },
+  {
+    title: 'Sticky Navbar',
+    description: "Sticky Navbar That Doesn't Go Away While You Scroll Down ",
+    imageUrl: './img/25.PNG',
+    link: './Projects/25-Sticky_Navbar/index.html',
+    number: '25'
+  },
+  {
+    title: 'Vertical Slider',
+    description: 'Vertical Slider Image Gallery Goes Up And Down ',
+    imageUrl: './img/26.PNG',
+    link: './Projects/26-Vertical_Slider/index.html',
+    number: '26'
+  },
+  {
+    title: 'Toast Notifications',
+    description: 'Button With Ever Click Shows A Small Notification Box Down The Screen ',
+    imageUrl: './img/27.PNG',
+    link: './Projects/27-Toast_Notifications/index.html',
+    number: '27'
+  },
+  {
+    title: 'Github Profile',
+    description: 'Search Any Github Username And Will Show You His Profile Information',
+    imageUrl: './img/28.PNG',
+    link: './Projects/28-Github_Profile/index.html',
+    number: '28'
+  },
+  {
+    title: 'Double Click Heart',
+    description: 'A Photo When You Click On It AnyWhere A Little Heart Icon Will Keep Showing Up ',
+    imageUrl: './img/29.PNG',
+    link: './Projects/29-Double_Click_Heart/index.html',
+    number: '29'
+  },
+  {
+    title: 'Auto Text Effect',
+    description: 'Auto Text Effect With A Box To Control How Speed You Want It To Go',
+    imageUrl: './img/30.PNG',
+    link: './Projects/30-Auto_Text_Effect/index.html',
+    number: '30'
+  },
+  {
+    title: 'Password Generator',
+    description: 'Password Generator Including Numbers & Symbols & Letters ',
+    imageUrl: './img/31.PNG',
+    link: './Projects/31-Password_Generator/index.html',
+    number: '31'
+  },
+  {
+    title: 'Toggle CheckBox',
+    description: 'Toggle Checkbox',
+    imageUrl: './img/32.PNG',
+    link: './Projects/32-Toggle_CheckBox/index.html',
+    number: '32'
+  },
+  {
+    title: 'Note App',
+    description: 'You Can Write Notes And Save Them To Local Storage And Edit Them And A Lot',
+    imageUrl: './img/33.PNG',
+    link: './Projects/33-Note_App/index.html',
+    number: '33'
+  },
+  {
+    title: 'Animated Count Down',
+    description: 'Count Down From 3 TO 1 With Nice Flipping Animation',
+    imageUrl: './img/34.PNG',
+    link: './Projects/34-Animation_Countdown/index.html',
+    number: '34'
+  },
+  {
+    title: 'Image Carousel',
+    description: 'Image Gallery With Auto Switching Between The Photos  ',
+    imageUrl: './img/35.PNG',
+    link: './Projects/35-Image_Carousel/index.html',
+    number: '35'
+  },
+  {
+    title: 'Hoverboard',
+    description: 'Small Boxes That Lights Up When You Hover Over It With Nice Colors',
+    imageUrl: './img/36.PNG',
+    link: './Projects/36-Hoverboard/index.html',
+    number: '36'
+  },
+  {
+    title: 'Pokedex',
+    description: 'Fetching Many Card With Every Pokemon All Information About Them And Special Powers',
+    imageUrl: './img/37.PNG',
+    link: './Projects/37-Pokedex/index.html',
+    number: '37'
+  },
+  {
+    title: 'Mobile Navigation',
+    description: 'Small Post Card Like Instagram With Small Navigation Tab Under It',
+    imageUrl: './img/38.PNG',
+    link: './Projects/38-Mobile_Tab_Navigation/index.html',
+    number: '38'
+  },
+  {
+    title: 'Password Strength',
+    description: 'Blurry Background ',
+    imageUrl: './img/39.PNG',
+    link: './Projects/39-Password_Strength_Background/index.html',
+    number: '39'
+  },
+  {
+    title: '3D Boxes',
+    description: 'Big 3D Box That Can Divide Into Mini 3D Boxes With One Button Click',
+    imageUrl: './img/40.PNG',
+    link: './Projects/40-3D_Boxes_Background/index.html',
+    number: '40'
+  },
+  {
+    title: 'Verify Account Ui',
+    description: 'Input With 6 Boxes To Enter A verification number (Design Only)',
+    imageUrl: './img/41.PNG',
+    link: './Projects/41-Verify_Account_Ui/index.html',
+    number: '41'
+  },
+  {
+    title: 'Live User Filter',
+    description: 'Fetch Users And Search Any User Account And Look Through His Information',
+    imageUrl: './img/42.PNG',
+    link: './Projects/42-Live_User_Filter/index.html',
+    number: '42'
+  },
+  {
+    title: 'Feedback Ui Design',
+    description: 'Feedback Design With 3 Emoji Chose Any One Between Them',
+    imageUrl: './img/43.PNG',
+    link: './Projects/43-Feedback_Ui_Design/index.html',
+    number: '43'
+  },
+  {
+    title: 'Rang Slider',
+    description: 'Custom Range Slider Choose A Number Between 0 & 100',
+    imageUrl: './img/44.PNG',
+    link: './Projects/44-Custom_Range_Slider/index.html',
+    number: '44'
+  },
+  {
+    title: 'Netflix Navigation',
+    description: 'Navigate Between The Menus With Smooth Animation',
+    imageUrl: './img/45.PNG',
+    link: './Projects/45-Netflix_Range_Slider/index.html',
+    number: '45'
+  },
+  {
+    title: 'Quiz App',
+    description: 'Quiz App With Some Questions And A Score With How Much Correct Answers You Got',
+    imageUrl: './img/46.PNG',
+    link: './Projects/46-Quiz_App/index.html',
+    number: '46'
+  },
+  {
+    title: 'Box Switcher',
+    description: 'Testimonial Box With A Information About An Employer',
+    imageUrl: './img/47.PNG',
+    link: './Projects/47-Testimonial_Box_Switcher/index.html',
+    number: '47'
+  },
+  {
+    title: 'Random Image Feed',
+    description: 'Fetching Random Images From Unsplash Every Time You Reload The Page',
+    imageUrl: './img/48.PNG',
+    link: './Projects/48-Random_Image_Feed/index.html',
+    number: '48'
+  },
+  {
+    title: 'Todo List',
+    description: 'Enter Mission You Wanna Complete And Toggle It To Mark It As Finished',
+    imageUrl: './img/49.PNG',
+    link: './Projects/49-Todo_List/index.html',
+    number: '49'
+  },
+  {
+    title: 'Catch The Insect',
+    description: 'Small Fun Little Game When You Can Hunt And Count How Many Insect Have You Caught',
+    imageUrl: './img/50.PNG',
+    link: './Projects/50-Catch_The_Insect/index.html',
+    number: '50'
+  },
+
+]
+// Create Boxes
+const boxesContainer = document.getElementById('project-container')
+
+const createBoxes = (projects) => {
+  const div = document.createElement('div')
+  div.classList.add('box')
+  const { title, description, imageUrl, link, number } = projects
+  div.innerHTML = `
+        <img src="${imageUrl}" alt="" />
+        <h2>${title}</h2>
+        <h4>${number}</h4>
+      <p>
+         ${description}
+      </p>
+      <a href="${link}"
+      target="_blank"><button class="btn">Visit</button></a
+      >
+  `
+
+  boxesContainer.appendChild(div)
+}
+
+projects.map((projects) => createBoxes(projects))
+
+
+// Auto Text Effect
+
+let typed = new Typed(".auto-type", {
+  strings: ["In 50 Days"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true,
+});
+
+
+
+// Navbar Animation
+const nav = document.getElementById('nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() { (window.scrollY > nav.offsetHeight + 800) ? nav.classList.add('active') : nav.classList.remove('active') }
